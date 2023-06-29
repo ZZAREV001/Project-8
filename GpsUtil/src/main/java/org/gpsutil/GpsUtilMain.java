7babca67ccbd2d37f627cf3d0da3c98b57824d5b
ZZAREV001 <zarev.zahari@gmail.com>
2023-06-29 16:18:34 +0200

    writing the entry point of the GpsUtil microservice

---
+++ GpsUtil/src/main/java/org/gpsutil/GpsUtilMain.java
@@ -0,0 +1,11 @@
+package org.gpsutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsUtilMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GpsUtilMain.class, args);
+    }
+}
